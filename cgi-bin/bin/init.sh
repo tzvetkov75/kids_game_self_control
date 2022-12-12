ce1ef0bc7f9a76dbda3d509d416b306ef46960c1
Vesselin Tzvetkov <tzvetkov75@users.noreply.github.com>
2022-12-12 22:53:48 +0100

    fixing init.sh to double the time if positive as

--- cgi-bin/bin/init.sh
+++ cgi-bin/bin/init.sh
@@ -1,13 +1,27 @@
-week_to_play_seconds="63000" # 2.5 x 7 x 3600
+week_to_play_seconds="72000" # 2.5 x 7 x 3600
 
 date +"%s" > ./db/ps4_stop_date.txt
 date +"%s" > ./db/phone_stop_date.txt
 echo $week_to_play_seconds > ./db/total.txt
 
+
+logger -p local0.notice "CaleControl: Starting Init"
 if test -f "./db/rest.txt"; then 
-	rest=$(cat ./db/rest.txt) 
+	rest=$(cat ./db/rest.txt)
+	logger -p local0.notice "CaleControl: Rest time [$rest] min"
+	# if time is save then double the time
+        if ((rest>0)); then 
+		rest=$((rest*2)); 
+		logger -p local0.notice "CaleControl: Dubble time [$rest] min since saved :-)"
+       	else 
+		rest="0"
+	fi
+
 	rest=$(($rest + $week_to_play_seconds))
 	echo $rest > ./db/rest.txt
 else
 	echo $week_to_play_seconds > ./db/rest.txt
 fi 
+
+rest=$(cat ./db/rest.txt)
+logger -p local0.notice "CaleControl: Total time now [$rest] min"
